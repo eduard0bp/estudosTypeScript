d668be8173db0240eeff94b48ef20d8411779f34
Eduardo Batista Pereira <duuhpwn@gmail.com>
2022-09-15 14:28:48 -0300

    Dia 4
    Estudo sobre classes, heranças, getter e setters

--- classes/classes.js
+++ classes/classes.js
@@ -63,4 +63,81 @@ caneta.desconto = 0.5;
 console.log(caneta.resumo());
 const carregador = new Produto('Carregador', 45.99);
 console.log(carregador.resumo());
+// Public & Private
+class Carro {
+    marca;
+    modelo;
+    velocidadeMaxima;
+    velocidadeAtual = 0; // Visível na própria classe e não transmitido por herança
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima;
+        if (velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+const carro1 = new Carro('Ford', 'Ka', 185);
+Array(50).fill(0).forEach(() => carro1.acelerar());
+console.log(carro1.acelerar());
+Array(40).fill(0).forEach(() => carro1.frear());
+console.log(carro1.frear());
+// Simulando "Erros"
+// carro1.velocidadeAtual = 300 // É permitido o acesso porque o Javascript não possúi classes Privadas
+// console.log(`Atual -> ${carro1.velocidadeAtual}`)
+// carro1.velocidadeMaxima = 500
+// console.log(`Máxima -> ${carro1.velocidadeMaxima}`)
+// carro1.alterarVelocidade(150)
+// console.log(`Atual -> ${carro1.velocidadeAtual}`)
+// Heranças
+// Public, Protect e Private
+class Ferrari extends Carro {
+    constructor(modelo, velocidadeMaxima) {
+        super('Ferrari', modelo, velocidadeMaxima);
+    }
+    acelerar() {
+        return this.alterarVelocidade(20);
+    }
+    frear() {
+        return this.alterarVelocidade(-15);
+    }
+}
+const f40 = new Ferrari('F40', 324);
+console.log(`${f40.marca}, ${f40.modelo}`);
+console.log(f40.acelerar());
+console.log(f40.acelerar());
+console.log(f40.frear());
+console.log(f40.frear());
+// Getter & Setters
+class Pessoa {
+    _idade = 0; // É utilizado o "_" como convenção para definir um atributo privado
+    get idade() {
+        return this._idade;
+    }
+    set idade(valor) {
+        if (valor >= 0 && valor <= 120) {
+            this._idade = valor;
+        }
+    }
+}
+const pessoa1 = new Pessoa;
+pessoa1.idade = 10;
+console.log(pessoa1.idade);
+pessoa1.idade = -3;
+console.log(pessoa1.idade);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -71,3 +71,99 @@ console.log(caneta.resumo())
 
 const carregador = new Produto('Carregador', 45.99)
 console.log(carregador.resumo())
+
+// Public & Private
+
+class Carro {
+  private velocidadeAtual: number = 0 // Visível na própria classe e não transmitido por herança
+
+  constructor(public marca: string, public modelo: string, private velocidadeMaxima: number = 200) {
+
+  }
+
+  protected alterarVelocidade(delta: number): number { // Visível na própria classe e transmitido por herança
+    const novaVelocidade = this.velocidadeAtual + delta
+    const velocidadeValida  = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+
+    if(velocidadeValida) {
+      this.velocidadeAtual = novaVelocidade
+    } else {
+      this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+    }
+
+    return this.velocidadeAtual
+  }
+
+  public acelerar(): number { // Visível fora da classe e transmitido por herança
+    return this.alterarVelocidade(5)
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-5)
+  }
+}
+
+const carro1 = new Carro('Ford', 'Ka', 185)
+Array(50).fill(0).forEach(() => carro1.acelerar())
+console.log(carro1.acelerar())
+
+Array(40).fill(0).forEach(() => carro1.frear())
+console.log(carro1.frear())
+
+// Simulando "Erros"
+
+// carro1.velocidadeAtual = 300 // É permitido o acesso porque o Javascript não possúi classes Privadas
+// console.log(`Atual -> ${carro1.velocidadeAtual}`)
+
+// carro1.velocidadeMaxima = 500
+// console.log(`Máxima -> ${carro1.velocidadeMaxima}`)
+
+// carro1.alterarVelocidade(150)
+// console.log(`Atual -> ${carro1.velocidadeAtual}`)
+
+// Heranças
+// Public, Protect e Private
+
+class Ferrari extends Carro {
+  constructor(modelo: string, velocidadeMaxima: number) {
+    super('Ferrari', modelo, velocidadeMaxima)
+  }
+
+  public acelerar(): number {
+    return this.alterarVelocidade(20)
+  }
+
+  public frear(): number {
+    return this.alterarVelocidade(-15)
+  }
+}
+
+const f40 = new Ferrari('F40', 324)
+console.log(`${f40.marca}, ${f40.modelo}`)
+console.log(f40.acelerar())
+console.log(f40.acelerar())
+console.log(f40.frear())
+console.log(f40.frear())
+
+// Getter & Setters
+
+class Pessoa {
+  private _idade: number = 0 // É utilizado o "_" como convenção para definir um atributo privado
+
+  get idade(): number {
+    return this._idade
+  }
+
+  set idade(valor: number) {
+    if(valor >= 0 && valor <= 120) {
+      this._idade = valor
+    }
+  }
+}
+
+const pessoa1 = new Pessoa
+pessoa1.idade = 10
+console.log(pessoa1.idade)
+
+pessoa1.idade = -3
+console.log(pessoa1.idade)
